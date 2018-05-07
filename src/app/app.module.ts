a4a111b5fceaa70de453198d8292d244cec2df37
Gerald Cantor <gcantor@cisco.com>
2018-05-06 20:52:05 -0500

    Configuring Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule,
+         MatCheckboxModule, 
+         MatDatepickerModule, 
+         MatFormFieldModule,
+         MatInputModule, 
+         MatRadioModule, 
+         MatSelectModule, 
+         MatSliderModule,
+         MatSlideToggleModule, 
+         MatToolbarModule, 
+         MatListModule, 
+         MatGridListModule,
+         MatCardModule, 
+         MatIconModule, 
+         MatProgressSpinnerModule, 
+         MatDialogModule} from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule, 
+    MatDatepickerModule, 
+    MatFormFieldModule,
+    MatInputModule, 
+    MatRadioModule, 
+    MatSelectModule, 
+    MatSliderModule,
+    MatSlideToggleModule, 
+    MatToolbarModule, 
+    MatListModule, 
+    MatGridListModule,
+    MatCardModule, 
+    MatIconModule, 
+    MatProgressSpinnerModule, 
+    MatDialogModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
