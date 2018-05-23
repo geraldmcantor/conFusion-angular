e8ee20715e76c37aeb39d6ea020a1a8a3c5d8a2d
Gerald Cantor <gcantor@cisco.com>
2018-05-23 07:52:45 -0500

    Promise Part 1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -9,11 +9,11 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Leader[] {
-      return LEADERS;
+  getLeaders(): Promise<Leader[]> {
+      return Promise.resolve(LEADERS);
   }
 
-  getFeaturedLeader(): Leader {
-    return LEADERS.filter((leader) => leader.featured)[0];
+  getFeaturedLeader(): Promise<Leader> {
+    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
   }
 }
